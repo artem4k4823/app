0d725a37477242fe570267bb623239a691b5005e
artem4k4823 <artemdv2000@gmail.com>
2026-01-05 22:37:30 +0700

    add login with token

--- api_v1/auth_veiws.py
+++ api_v1/auth_veiws.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.database import db
 from app.schemas.auth_shema import AuthUser
 from app.crud.user import get_user_by_usernames
-from app.core.auth_helper import verify_password, hash_password
+from app.core.auth_helper import verify_password, hash_password, create_jwt
+from fastapi.security import OAuth2PasswordRequestForm
+
 router = APIRouter(prefix='/log', tags=['OAuth'])
 
 @router.post('/login')
@@ -13,3 +15,33 @@ async def aunthenticate_user(session: Annotated[AsyncSession, Depends(db.session
     if not user or not verify_password(user.password, db_user.password):
         return False
     return user
+
+@router.post('/token-login')
+async def login( session:Annotated[AsyncSession,Depends(db.session_getter)], data: OAuth2PasswordRequestForm = Depends(), ):
+    user = await get_user_by_usernames(session=session,username=data.username)
+    
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверный логин или пароль",
+        )
+    
+    
+    if not verify_password(data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверный логин или пароль",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        
+    if not user.status:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Пользователь неактивен"
+        )
+    access_token = create_jwt(data={"sub": user.username})
+    
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
--- schemas/auth_shema.py
+++ schemas/auth_shema.py
@@ -3,4 +3,4 @@ from pydantic import BaseModel
 class AuthUser(BaseModel):
     username: str
     password: str
-    is_admin:bool = False
+    
